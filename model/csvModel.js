1f4410e1f30de506dce3845aca551a6e64197abe
Daniel Chan <daniel.chan@travelport.com>
2017-05-23 10:48:18 +0800

    refined csv header fields

--- model/csvModel.js
+++ model/csvModel.js
@@ -8,33 +8,25 @@ const CSVModel = [
     'market', // HKGBKK - Hong Kong to BangKok
     'OTA', // ExpediaSG
     'type', // economy
+    'departDate', // 08/16/16
+    'returnDate', // 08/19/16
 
     // DEPART
     'departFlightNo', // 110
     'departAirlineCode', // CX
     'departRoute', // 15:20 - HKG | 20:05 - KIX
-    'departDuration', // 1:30
-    'departDate', // 08/16/16
-    'departTime', // 11:10
 
     // RETURN
     'returnFlightNo', // 121
     'returnAirlineCode', // CX
     'returnRoute', // 06:30 - KIX / 07:40 - HND | 08:55 - HND / 12:30 - HKG
-    'returnDuration', // 1:45
-    'returnDate', // 08/19/16
-    'returnTime', // 18:10
 ];
 
 /* 
     NOTE: Can be added
     
     'passengers' // 1
-
-    'departRoute', // 07:55 - HKG :: 12:25 - ICN
     'departFlight', // CX414
-
-    'returnRoute', // 08:45 - ICN :: 11:25 - HKG
     'returnFlight', // CX415
 
 */
